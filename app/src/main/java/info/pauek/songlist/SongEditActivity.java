0a7ca023eef17b02bf18093515587b42deccf389
Siitoo <alfonsosanchez1896@gmail.com>
2018-10-27 19:23:29 +0200

    6. Initialitza l'any de SongEditActivity a l'any actual

--- app/src/main/java/info/pauek/songlist/SongEditActivity.java
+++ app/src/main/java/info/pauek/songlist/SongEditActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class SongEditActivity extends AppCompatActivity {
 
     EditText edit_year;
@@ -16,7 +19,11 @@ public class SongEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_song_edit);
 
         edit_year = findViewById(R.id.edit_year);
-        edit_year.setText("");
+
+
+        Calendar calendar = new GregorianCalendar();
+        int year = calendar.get(Calendar.YEAR);
+        edit_year.setText(Integer.toString(year));
     }
 
     public void onMinusClick(View view) {
